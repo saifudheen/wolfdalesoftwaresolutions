f3a430db1cc7469d7fa3a7bd3199f94c7bd75184
Saifudheen Aroniyil <Saifudheen.Aroniyil@gmail.com>
2016-09-30 05:10:08 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,6 +33,12 @@ app.get('/demo', function (req, res) {
                  console.log('error fetching add item: ');
                 res.end( '[{ "RESULT" : "0"}]');
 }); 
+
+
+app.all('/*',function(req,res,next){
+  res.sendfile('client/index.html',{route: __dirname });
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
