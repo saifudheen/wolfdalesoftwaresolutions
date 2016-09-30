851babb680986b014422e57ed8b8c97bbcd8560c
Saifudheen Aroniyil <Saifudheen.Aroniyil@gmail.com>
2016-09-30 05:06:55 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.configure(function() {
     app.use(express.static(__dirname + "/client"));
 });
 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log("Express server listening on port " + app.get('port'));
-});
+// http.createServer(app).listen(app.get('port'), function() {
+//     console.log("Express server listening on port " + app.get('port'));
+// });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -33,7 +33,7 @@ app.get('/demo', function (req, res) {
                  console.log('error fetching add item: ');
                 res.end( '[{ "RESULT" : "0"}]');
 }); 
-// var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-// app.listen(8080, function () {
-//   console.log(`IMAD course app listening on port ${port}!`);
-// });
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
