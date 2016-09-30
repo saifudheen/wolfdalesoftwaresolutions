8295b2c7423ecaa7446ebe48432db8526ab8e373
Saifudheen Aroniyil <Saifudheen.Aroniyil@gmail.com>
2016-09-30 05:08:14 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var http = require('http');
 
 var app = express();
 
-app.configure(function() {
-    app.set('port', process.env.PORT || 8080)
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
+// app.configure(function() {
+//     app.set('port', process.env.PORT || 8080)
+//     app.use(express.bodyParser());
+//     app.use(express.methodOverride());
 
-    app.use(app.router);
-    app.use(express.static(__dirname + "/client"));
-});
+//     app.use(app.router);
+//     app.use(express.static(__dirname + "/client"));
+// });
 
 // http.createServer(app).listen(app.get('port'), function() {
 //     console.log("Express server listening on port " + app.get('port'));
